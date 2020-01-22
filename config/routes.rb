eca9d424c28c19ca33c5de76adf2e669d74c441d
erknssgrrmns <0wd38c1x1334w9j@gmail.com>
2020-01-22 20:51:57 +0900

    edit side bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root "groups#index"
   resources :users, only: [:edit, :update]
+<<<<<<< HEAD
+=======
+  resources :groups,only: [:index, :new, :create, :edit, :update]
+>>>>>>> parent of 33e5fb8... Revert "edit side bar"
 end 
