2e5033e257f5e690d6d7f80b766cbe54c8a270d8
erknssgrrmns <0wd38c1x1334w9j@gmail.com>
2020-01-29 15:07:42 +0900

    create automatic updating system

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -63,6 +63,76 @@ $('#new_message').on('submit', function(e){
     .fail(function() {
       alert("メッセージ送信に失敗しました");
     });
-
-})
+  });
+  
+  
+  var buildHTML = function(message) {
+    if ( message.image ) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+           <div class="message__user-data">
+             <div class="message__user-data--talker">
+               ${message.user_name}
+             </div>
+             <div class="message__user-data--date">
+               ${message.created_at}
+             </div>
+           </div>
+           <div class="message__content">
+             <div class="message__content--text">
+               ${message.content}
+             </div>
+             <div class="message__content--image"> 
+               <img src=${message.image} >
+             </div>   
+           </div>
+         </div>`
+      return html;
+    } else {
+      var html =
+       `<div class="message" data-message-id=${message.id}>
+          <div class="message__user-data">
+            <div class="message__user-data--talker">
+              ${message.user_name}
+            </div>
+            <div class="message__user-data--date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__content">
+            <div class="message__content--text">
+              ${message.content}
+            </div>
+          </div>
+        </div>`
+      return html;
+    };
+  };
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.chat-main__message-list').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
